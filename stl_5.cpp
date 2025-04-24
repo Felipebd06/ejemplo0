afe04fce10a10154f82dffecd2df66c2b7730365
felipe.barrera-d <felipe.barrera-d@mail.escuelaing.edu.co>
2025-04-23 23:00:25 -0500

    quinto

---
+++ stl_5.cpp
@@ -0,0 +1,8 @@
+Ordenamiento personalizado: Ordena un vector<pair<int, string>> según el valor numérico de manera descendente.
+std::vector<std::pair<int, std::string>> datos = {
+    {5, "manzana"},
+    {2, "banana"},
+    {8, "naranja"},
+    {1, "uva"},
+    {7, "pera"}
+};
